3147ad5058533505136a1a896b11b9da5bda4ee3
Adam Hutson <adam.hutson@yahoo.com>
2018-03-06 15:49:05 -0600

    update folder str

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -3,8 +3,9 @@ USERNAME=$1
 TOKEN$2
 ORGANIZATION=$3
 
-mkdir -p ~/github.com/$3/repos
-cd ~/github.com/$3/repos
+mkdir -p ~/github.com/$3
+cd ~/github.com/$3
+
 for i in `curl -u $1:$2 https://api.github.com/orgs/$3/repos | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
 do git clone $i
 done
