881291062bc0b881110e70fad3db264344e0263d
Adam Hutson <adam.hutson@yahoo.com>
2018-03-06 15:45:52 -0600

    update org parameter

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -5,6 +5,6 @@ ORGANIZATION=$3
 
 mkdir -p ~/github.com/$3/repos
 cd ~/github.com/$3/repos
-for i in `curl -u $1:$2 https://api.github.com/orgs/confluentinc/repos | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
+for i in `curl -u $1:$2 https://api.github.com/orgs/$3/repos | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
 do git clone $i
 done
