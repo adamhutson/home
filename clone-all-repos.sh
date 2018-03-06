4c500605bcdf24e3308b23e453ee1a64da79b133
Adam Hutson <adam.hutson@yahoo.com>
2018-03-06 15:57:10 -0600

    add per_page param

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -6,6 +6,6 @@ ORGANIZATION=$3
 mkdir -p ~/github.com/$3
 cd ~/github.com/$3
 
-for i in `curl -u $1:$2 https://api.github.com/orgs/$3/repos | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
+for i in `curl -u $1:$2 https://api.github.com/orgs/$3/repos?per_page=200 | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
 do git clone $i
 done
