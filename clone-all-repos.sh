3872068c5139f9f309faceaa4e5665b87f97ccda
Adam Hutson <adam.hutson@yahoo.com>
2018-04-18 16:21:49 -0400

    added pagination to repos api call

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
+# to execute: $ bash clone-all-repos.sh your_username your_token desired_org
+
+# more documentation at https://developer.github.com/v3
 USERNAME=$1
-TOKEN$2
+TOKEN=$2
 ORGANIZATION=$3
 
 mkdir -p ~/github.com/$3
 cd ~/github.com/$3
 
-for i in `curl -u $1:$2 https://api.github.com/orgs/$3/repos?per_page=200 | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
-do git clone $i
+total_repos=`curl -u $1:$2 https://api.github.com/orgs/$3 | grep total_private_repos | cut -d ':' -f 2-3|tr -d '",'`
+
+page=0
+for repo in `seq 1 30 $total_repos`
+do
+  page=$(($page+1))
+  for i in `curl -u $1:$2 https://api.github.com/orgs/$3/repos?page=$page | grep clone_url | cut -d ':' -f 2-3|tr -d '",'`
+    do git clone $i
+  done  
 done
+
