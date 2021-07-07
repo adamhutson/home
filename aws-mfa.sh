8b495b42417bcfd3af6e1f3809c184dff0222662
Adam Hutson <adam.hutson@yahoo.com>
2021-07-07 08:26:31 -0500

    awscli mfa script

---
+++ aws-mfa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MY_MFA_DEVICE=<my-mfa-device-arn>
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: ${0} <my-profile> <my-mfa-token>"
+    echo "  Configures a new profile, <my-profile>-mfa, with session"
+    echo "Example: ${0} dev 123456"
+    echo "  Produces profile 'dev-mfa'"
+    echo "  aws s3 ls --profile dev-mfa"
+    exit 2
+fi
+function set-mfa {
+    aws configure set region $2 --profile $1-mfa
+    aws configure set aws_access_key_id $3 --profile $1-mfa
+    aws configure set aws_secret_access_key $4 --profile $1-mfa
+    aws configure set aws_session_token $5 --profile $1-mfa
+}
+set-mfa $1 $(aws configure get region --profile $1) $(aws sts get-session-token --profile $1 --serial-number $MY_MFA_DEVICE --token-code $2 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text)
