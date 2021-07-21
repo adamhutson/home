928e61256e816850f639812c35a953ec1caff06f
Adam Hutson <adam.hutson@narvar.com>
2021-07-21 15:33:43 -0500

    update mfa script to echo results

--- aws-mfa.sh
+++ aws-mfa.sh
@@ -16,5 +16,6 @@ function set-mfa {
     aws configure set aws_access_key_id $3 --profile $1-mfa
     aws configure set aws_secret_access_key $4 --profile $1-mfa
     aws configure set aws_session_token $5 --profile $1-mfa
+    echo "Configured profile '${1}-mfa', with session"
 }
 set-mfa $1 $(aws configure get region --profile $1) $(aws sts get-session-token --profile $1 --serial-number $MY_MFA_DEVICE --token-code $2 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text)
